1c7fbcc7f16b379b748828b99af20d1fab55e545
Maxim <vedernikovmaxim@rambler.ru>
2023-03-18 14:31:47 +0300

    update seminar_three

--- s_3/collektions.py
+++ s_3/collektions.py
@@ -1,6 +1,7 @@
 import operator
 import string
 from itertools import combinations
+from collections import Counter
 
 
 # Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
@@ -12,6 +13,33 @@ def search_duplicates(primary_list: list):
         if buf in primary_list:
             res_list.append(buf)
     return res_list
+
+
+def search_duplicates_v2(pr_list: list):
+    counter = {}
+    res_list = []
+    for item in pr_list:
+        counter[item] = counter.get(item, 0) + 1
+    for key, value in counter.items():
+        if counter[key] > 1:
+            res_list.append(key)
+    return res_list
+
+
+def search_duplicates_v3(pr_list: list):
+    res_list = []
+    counter = Counter(pr_list)
+    for key, value in counter.items():
+        if counter[key] > 1:
+            res_list.append(key)
+    return res_list
+
+
+# линейная сожность
+def search_duplicates_v4(pl_list: list):
+    vis = set()
+    res = [x for x in pl_list if x in vis or (vis.add(x))]
+    return res
 
 
 # В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
@@ -49,13 +77,27 @@ def shove_things(dict_things: dict, load_capacity: int):
         print(i)
 
 
+def shove_things_v2(dict_things: dict, load_capacity: int):
+    def find(dict_things_: dict, load_capacity_: int, flag):
+        db = {flag: dict_things_[flag]}
+        dict_things_.pop(flag)
+        sum = db[flag]
+        for key_ in dict_things_.keys():
+            if sum + dict_things_[key_] <= load_capacity_:
+                db[key_] = dict_things_[key_]
+        return db
+
+    res_list = []
+    for key in dict_things.keys():
+        res_list.append(find(dict_things, load_capacity, key))
+
+
 if __name__ == '__main__':
-    # w = [1, 3, 5, 7, 5, 3, 1]
-    # print(search_duplicates(w))
-    # text = 'Временная сложность сортировки словаря с использованием функции itemgetter() равна O(n log n).' \
-    #        'Это связано с тем, что функция itemgetter() должна проходить через словарь и сравнивать каждый элемент,' \
-    #        'чтобы определить порядок сортировки. Поскольку сравнение каждого элемента - это операция O (n), а' \
-    #        'сортировка - операция O (log n), общая временная сложность составляет O (n log n).'
-    # print(frequent_words(text))
-    test = {'1': 3, '2': 5, '3': 2, '4': 7}
-    shove_things(test, 10)
+    w = [1, 3, 5, 7, 5, 3, 1]
+    print(search_duplicates_v4(w))
+    text = 'Временная сложность сортировки словаря с использованием функции itemgetter() равна O(n log n).' \
+           'Это связано с тем, что функция itemgetter() должна проходить через словарь и сравнивать каждый элемент,' \
+           'чтобы определить порядок сортировки. Поскольку сравнение каждого элемента - это операция O (n), а' \
+           'сортировка - операция O (log n), общая временная сложность составляет O (n log n).'
+    print(frequent_words(text))
+
